2f90406eb19a2ddbe7c3e37cf7b758fc3b949e58
Maja Celej <cmaja1825@gmail.com>
2024-04-23 09:23:15 +0200

    created Icon index

---
+++ src/common/Navigation/Icon/index.js
@@ -0,0 +1,10 @@
+import { Wrapper } from "./styled";
+import { ReactComponent as MovieIcon } from './CameraIcon.svg';
+
+export const Icon = () => {
+	return (
+		<Wrapper>
+			<MovieIcon />
+		</Wrapper>
+	);
+};
