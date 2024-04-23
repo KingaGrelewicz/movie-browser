2d92c70727e205e43f1e3fb7410a9c080e4acbcb
Maja Celej <cmaja1825@gmail.com>
2024-04-23 09:25:00 +0200

    added Navigation component to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Globalstyle } from "./GlobalStyle";
 import Container from "./common/Container";
+import Navigation from "./common/Navigation";
 
 function App() {
   return (
     <Container>
       <Globalstyle />
+      <Navigation />
       <header>Movies Browser</header>
     </Container>
   );
