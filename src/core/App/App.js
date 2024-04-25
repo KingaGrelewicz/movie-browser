14d6c39c377d9cf1517e26182500b54b5b8f58d9
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-04-25 13:12:41 +0200

    ThemeProvider moved to App.js

--- src/core/App/App.js
+++ src/core/App/App.js
@@ -2,15 +2,21 @@ import { Globalstyle } from "./GlobalStyle";
 import Container from "../../common/Container/styled";
 import { PopularMovies } from "../../features/MoviesBrowser/PopularMovies/Popular";
 import Navigation from "../../common/Navigation";
+import MovieDetails from "../../features/MoviesBrowser/MovieDetails";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme";
 
 function App() {
   return (
     <>
-      <Globalstyle />
-      <Navigation />
-      <Container>
-        <PopularMovies />
-      </Container>
+      <ThemeProvider theme={theme}>
+        <Globalstyle />
+        <Navigation />
+        <Container>
+          <PopularMovies />
+          <MovieDetails />
+        </Container>
+      </ThemeProvider>
     </>
   );
 }
