892b6ddb293e894e5990a3e60e5f1a9ef8941647
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-04-25 13:14:25 +0200

    MovieDetails Component created

---
+++ src/features/MoviesBrowser/MovieDetails/index.js
@@ -0,0 +1,33 @@
+import { useDispatch, useSelector } from "react-redux";
+
+import {
+  fetchMovieDetails,
+  selectMovieDetailsSlice,
+} from "./movieDetailsSlice";
+import { useEffect } from "react";
+import Details from "./Details";
+import BackdropView from "./BackdropView";
+
+const MovieDetails = ({ imageUrl }) => {
+  const dispatch = useDispatch();
+  const movieDetails = useSelector(selectMovieDetailsSlice);
+
+  useEffect(() => {
+    dispatch(fetchMovieDetails());
+  }, [dispatch]);
+
+  return (
+    <div>
+      {movieDetails.status === "loading" && <p>Loading...</p>}
+      {movieDetails.status === "success" && (
+        <>
+          <BackdropView imageUrl={imageUrl} />
+          <Details />
+        </>
+      )}
+      {movieDetails.status === "error" && <p>Error fetching data</p>}
+    </div>
+  );
+};
+
+export default MovieDetails;
