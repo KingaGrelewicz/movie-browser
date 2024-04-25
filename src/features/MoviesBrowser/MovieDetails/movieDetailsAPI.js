20bcbac89de11c6e47985d880f9f255ef15f4611
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-04-25 13:13:24 +0200

    movieDetailsAPI created

---
+++ src/features/MoviesBrowser/MovieDetails/movieDetailsAPI.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+async function getMovieDetails() {
+  const APIkey = "b024809636d033af8d3a072db3fe8b5d";
+  const response = await axios.get(
+    `https://api.themoviedb.org/3/movie/693134?api_key=${APIkey}`
+  );
+  console.log(response);
+  return response.data;
+}
+
+export default getMovieDetails;
