338bd22e84a68c43adbbd0e5ec8561ec8bc2b1ed
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-04-25 10:04:52 +0200

    Popular Movie Component created

---
+++ src/features/MoviesBrowser/PopularMovies/Popular/styled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+max-width: 1368px;
+margin: auto;
+`;
+
+export const Title = styled.h1`
+font-weight: 600;
+font-size: 36px;
+`;
+
+export const Tile = styled.section`
+display: grid;
+grid-template-columns: 1fr 1fr 1fr 1fr;
+grid-gap: 24px
+`
