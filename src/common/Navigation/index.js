d832d17cbcac82a8ae61446a447757a7a5366107
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-04-25 10:03:54 +0200

    Navigaition created

--- src/common/Navigation/index.js
+++ src/common/Navigation/index.js
@@ -2,24 +2,26 @@ import Search from "./Search";
 import { Wrapper, List, Item, NavLink, PageTitle } from "./styled";
 import { Icon } from "./Icon";
 
-export default () => {
-	return (
-		<nav>
-			<List>
-				<Wrapper>
-					<PageTitle>
-						<Icon />
-						Movies Browser
-					</PageTitle>
-					<Item>
-						<NavLink>MOVIES</NavLink>
-					</Item>
-					<Item>
-						<NavLink>PEOPLE</NavLink>
-					</Item>
-				</Wrapper>
-				<Search />
-			</List>
-		</nav>
-	);
+const Navigation = () => {
+  return (
+    <nav>
+      <List>
+        <Wrapper>
+          <PageTitle>
+            <Icon />
+            Movies Browser
+          </PageTitle>
+          <Item>
+            <NavLink>MOVIES</NavLink>
+          </Item>
+          <Item>
+            <NavLink>PEOPLE</NavLink>
+          </Item>
+        </Wrapper>
+        <Search />
+      </List>
+    </nav>
+  );
 };
+
+export default Navigation;
--- src/common/Navigation/styled.js
+++ src/common/Navigation/styled.js
@@ -1,74 +1,74 @@
 import styled from "styled-components";
 
 export const List = styled.ul`
-	background: ${({ theme }) => theme.color.black};
-	margin-top: 0px;
-	list-style: none;
-	display: flex;
-	flex-direction: row;
-	align-items: center;
-	justify-content: space-around;
-	padding: 23px 0px;
+  background: ${({ theme }) => theme.color.black};
+  margin-top: 0px;
+  list-style: none;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  padding: 23px 0px;
 
-	@media (max-width: 991px) {
-		display: grid;
-		grid-row-gap: 24px;
-	}
+  @media (max-width: 991px) {
+    display: grid;
+    grid-row-gap: 24px;
+  }
 `;
 
 export const Wrapper = styled.span`
-	display: flex;
-	align-items: center;
-	justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-	@media (max-width: 767px) {
-		justify-content: baseline;
-	}
+  @media (max-width: 767px) {
+    justify-content: baseline;
+  }
 `;
 
 export const NavLink = styled.nav`
-	color: ${({ theme }) => theme.color.white};
-	text-decoration: none;
-	padding: 8px 24px;
-	font-size: 14px;
-	font-weight: 600;
-	line-height: 21px;
+  color: ${({ theme }) => theme.color.white};
+  text-decoration: none;
+  padding: 8px 24px;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 21px;
 
-	&:hover {
-		border: 1px solid ${({ theme }) => theme.color.white};
-		border-radius: 24px;
-	}
+  &:hover {
+    border: 1px solid ${({ theme }) => theme.color.white};
+    border-radius: 24px;
+  }
 
-	@media (max-width: 991px) {
-		font-size: 12px;
-		font-weight: 600;
-	}
+  @media (max-width: 991px) {
+    font-size: 12px;
+    font-weight: 600;
+  }
 `;
 
 export const Item = styled.li`
-	margin: 0px 8px;
+  margin: 0px 8px;
 
-	@media (max-width: 767px) {
-		margin: auto;
-	}
+  @media (max-width: 767px) {
+    margin: auto;
+  }
 `;
 
 export const PageTitle = styled.span`
-	font-size: 24px;
-	font-weight: 500;
-	line-height: 40px;
-	margin-right: 80px;
-	color: ${({ theme }) => theme.color.white};
-	display: flex;
-	align-items: center;
+  font-size: 24px;
+  font-weight: 500;
+  line-height: 40px;
+  margin-right: 80px;
+  color: ${({ theme }) => theme.color.white};
+  display: flex;
+  align-items: center;
 
-	@media (max-width: 1199px) {
-		margin-right: 20px;
-	}
+  @media (max-width: 1199px) {
+    margin-right: 20px;
+  }
 
-	@media (max-width: 767px) {
-		font-size: 13px;
-		font-weight: 500;
-		margin: auto;
-	}
+  @media (max-width: 767px) {
+    font-size: 13px;
+    font-weight: 500;
+    margin: auto;
+  }
 `;
