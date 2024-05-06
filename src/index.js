ce72e7a66bd16a1d5062d369c8a56a81006e3f8e
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-05-06 14:03:08 +0200

    paths corrected

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./core/App/App";
+import { App } from "./core/App/App";
 import reportWebVitals from "./reportWebVitals";
 import { Normalize } from "styled-normalize";
 import { Provider } from "react-redux";
 import store from "./core/App/store";
 import { ThemeProvider } from "styled-components";
-import { theme } from "./theme";
-import { Globalstyle } from "./GlobalStyle";
+import { theme } from "./core/App/theme";
+import { Globalstyle } from "./core/App/GlobalStyle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<Provider store={store}>
-		<ThemeProvider theme={theme}>
-			<Globalstyle />
-			<Normalize />
-			<React.StrictMode>
-				<App />
-			</React.StrictMode>
-		</ThemeProvider>
-	</Provider>
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <Globalstyle />
+      <Normalize />
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ThemeProvider>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
