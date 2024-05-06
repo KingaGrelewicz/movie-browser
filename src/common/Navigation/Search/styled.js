93978c63e476398687e4f873f9ab89f0107dbc36
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-05-06 14:03:36 +0200

    Media Query path added

--- src/common/Navigation/Search/styled.js
+++ src/common/Navigation/Search/styled.js
@@ -1,27 +1,28 @@
 import styled from "styled-components";
+import { mediaQuery } from "../../../core/App/theme";
 
 export const Wrapper = styled.div`
-	display: flex;
-	max-width: 450px;
-	flex-basis: 100%;
+  display: flex;
+  max-width: 450px;
+  flex-basis: 100%;
 
-	@media (max-width: ${({ mediaQuery }) => mediaQuery.breakpoints.tabletVerticalMax}px) {
-		justify-content: center;
-	}
+  @media (max-width: ${mediaQuery.breakpoints.tabletVerticalMax}px) {
+    justify-content: center;
+  }
 `;
 
 export const Input = styled.input`
-	background-color: ${({ theme }) => theme.color.white};
-	border: 1px solid ${({ theme }) => theme.color.grey};
-	border-radius: 33px;
-	font-size: 16px;
-	font-weight: 400;
-	line-height: 24px;
-	color: ${({ theme }) => theme.color.darkerGrey};
-	padding: 12px 24px;
-	flex-basis: 100%;
+  background-color: ${({ theme }) => theme.color.white};
+  border: 1px solid ${({ theme }) => theme.color.grey};
+  border-radius: 33px;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 24px;
+  color: ${({ theme }) => theme.color.darkerGrey};
+  padding: 12px 24px;
+  flex-basis: 100%;
 
-	@media (max-width: ${({ mediaQuery }) => mediaQuery.breakpoints.tabletVerticalMax}px) {
-		margin: 0px 20px;
-	}
+  @media (max-width: ${mediaQuery.breakpoints.tabletVerticalMax}px) {
+    margin: 0px 20px;
+  }
 `;
--- src/common/Navigation/styled.js
+++ src/common/Navigation/styled.js
@@ -1,84 +1,85 @@
 import styled from "styled-components";
-import { NavLink } from "react-router-dom;";
+import { mediaQuery } from "../../core/App/theme";
+import { NavLink } from "react-router-dom";
 
 const activeClassName = "link-active";
 
 export const StyledNavLink = styled(NavLink).attrs(() => ({
-	activeClassName,
+  activeClassName,
 }))`
-	color: ${({ theme }) => theme.color.white};
-	text-decoration: none;
-	padding: 8px 24px;
-	font-size: 14px;
-	font-weight: 600;
-	line-height: 21px;
+  color: ${({ theme }) => theme.color.white};
+  text-decoration: none;
+  padding: 8px 24px;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 21px;
 
-	&.${activeClassName} {
-		border: 1px solid ${({ theme }) => theme.color.white};
-		border-radius: 24px;
-	}
+  &.${activeClassName} {
+    border: 1px solid ${({ theme }) => theme.color.white};
+    border-radius: 24px;
+  }
 
-	&:hover {
-		border: 1px solid ${({ theme }) => theme.color.white};
-		border-radius: 24px;
-	}
+  &:hover {
+    border: 1px solid ${({ theme }) => theme.color.white};
+    border-radius: 24px;
+  }
 
-	@media (max-width: ${({ mediaQuery }) => mediaQuery.breakpoints.mobileMax}px) {
-		font-size: 12px;
-		font-weight: 600;
-	}
+  @media (max-width: ${mediaQuery.breakpoints.mobileMax}px) {
+    font-size: 12px;
+    font-weight: 600;
+  }
 `;
 
 export const Item = styled.li`
-	margin: 0px 8px;
+  margin: 0px 8px;
 
-	@media (max-width: ${({ mediaQuery }) => mediaQuery.breakpoints.mobileMax}px) {
-		margin: auto;
-	}
+  @media (max-width: ${mediaQuery.breakpoints.mobileMax}px) {
+    margin: auto;
+  }
 `;
 
 export const List = styled.ul`
-	background: ${({ theme }) => theme.color.black};
-	margin-top: 0px;
-	list-style: none;
-	display: flex;
-	flex-direction: row;
-	align-items: center;
-	justify-content: space-around;
-	padding: 23px 0px;
+  background: ${({ theme }) => theme.color.black};
+  margin-top: 0px;
+  list-style: none;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  padding: 23px 0px;
 
-	@media (max-width: ${({ mediaQuery }) => mediaQuery.breakpoints.tabletVerticalMax}px) {
-		display: grid;
-		grid-row-gap: 24px;
-	}
+  @media (max-width: ${mediaQuery.breakpoints.tabletVerticalMax}px) {
+    display: grid;
+    grid-row-gap: 24px;
+  }
 `;
 
 export const Wrapper = styled.span`
-	display: flex;
-	align-items: center;
-	justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-	@media (max-width: ${({ mediaQuery }) => mediaQuery.breakpoints.mobileMax}px) {
-		justify-content: baseline;
-	}
+  @media (max-width: ${mediaQuery.breakpoints.mobileMax}px) {
+    justify-content: baseline;
+  }
 `;
 
 export const PageTitle = styled.span`
-	font-size: 24px;
-	font-weight: 500;
-	line-height: 40px;
-	margin-right: 80px;
-	color: ${({ theme }) => theme.color.white};
-	display: flex;
-	align-items: center;
+  font-size: 24px;
+  font-weight: 500;
+  line-height: 40px;
+  margin-right: 80px;
+  color: ${({ theme }) => theme.color.white};
+  display: flex;
+  align-items: center;
 
-	@media (max-width: ${({ mediaQuery }) => mediaQuery.breakpoints.tabletHorizontalMax}px) {
-		margin-right: 20px;
-	}
+  @media (max-width: ${mediaQuery.breakpoints.tabletHorizontalMax}px) {
+    margin-right: 20px;
+  }
 
-	@media (max-width: ${({ mediaQuery }) => mediaQuery.breakpoints.mobileMax}px) {
-		font-size: 13px;
-		font-weight: 500;
-		margin: auto;
-	}
+  @media (max-width: ${mediaQuery.breakpoints.mobileMax}px) {
+    font-size: 13px;
+    font-weight: 500;
+    margin: auto;
+  }
 `;
