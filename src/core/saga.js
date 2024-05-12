48d500943f8a5578f49015fe59b9d874fb3ad134
Tomasz Samborski <tomek986@wp.pl>
2024-05-12 22:21:57 +0200

    add genre to moviebrowsers and create nwe image vision

--- src/core/saga.js
+++ src/core/saga.js
@@ -1,8 +1,10 @@
 import { all } from "redux-saga/effects";
 import { movieBrowserSaga } from "../features/MoviesBrowser/movieBrowserSaga";
+import { movieGenreSaga } from "../features/MoviesBrowser/PopularMovies/movieGenreSaga";
 
 export default function* saga() {
     yield all([
         movieBrowserSaga(),
+        movieGenreSaga(),
     ]);
 }
