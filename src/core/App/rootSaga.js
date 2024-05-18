85660315a45d8820245838552b403fc78d1089f8
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-05-18 15:13:00 +0200

    movieCredisSaga added to rootSaga

--- src/core/App/rootSaga.js
+++ src/core/App/rootSaga.js
@@ -1,6 +1,8 @@
 import { all } from "redux-saga/effects";
 import { movieDetailsSaga } from "../../features/MoviesBrowser/MovieDetails/movieDetailsSaga";
+import { popularPeopleSaga } from "../../features/MoviesBrowser/PopularPeople/popularPeopleSaga";
+import { movieCreditsSaga } from "../../features/MoviesBrowser/MovieDetails/Credits/creditsSaga";
 
 export default function* rootSaga() {
-  yield all([movieDetailsSaga()]);
+  yield all([movieDetailsSaga(), popularPeopleSaga(), movieCreditsSaga()]);
 }
