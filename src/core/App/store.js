390299fffb266f862920057a34f2c94ecb723d6d
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-05-18 15:13:59 +0200

    new reducers added to store

--- src/core/App/store.js
+++ src/core/App/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import movieDetailsReducer from "../../features/MoviesBrowser/MovieDetails/movieDetailsSlice";
+import popularPeopleReducer from "../../features/MoviesBrowser/PopularPeople/popularPeopleSlice";
+import movieCreditsReducer from "../../features/MoviesBrowser/MovieDetails/Credits/creditsSlice";
 import rootSaga from "./rootSaga";
 
 const sagaMiddleware = createSagaMiddleware();
@@ -8,6 +10,8 @@ const sagaMiddleware = createSagaMiddleware();
 const store = configureStore({
   reducer: {
     movieDetails: movieDetailsReducer,
+    popularPeople: popularPeopleReducer,
+    movieCredits: movieCreditsReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(sagaMiddleware),
