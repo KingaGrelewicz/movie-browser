87d7f02c659bbfad1adf2061e092a1c95d8e0486
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-05-18 15:11:56 +0200

    mediaQuery added

--- src/features/MoviesBrowser/MovieDetails/Backdrop/styled.js
+++ src/features/MoviesBrowser/MovieDetails/Backdrop/styled.js
@@ -1,28 +1,70 @@
 import styled from "styled-components";
+import { mediaQuery } from "../../../../core/App/theme";
 
 export const BackdropHeader = styled.header`
   background: ${({ theme }) => theme.color.black};
 `;
 
-export const BacdropContainer = styled.div`
+export const BackdropContainer = styled.div`
   max-width: 1368px;
   margin: 0 auto;
   position: relative;
+  overflow: hidden;
+
+  @media (max-width: ${mediaQuery.breakpoints.mobileMax}px) {
+  }
+
+  @media (max-width: ${mediaQuery.breakpoints.tabletVerticalMax}px) {
+  }
 `;
 
 export const BackdropImg = styled.img`
-  position: relative;
   display: block;
   margin: 0 auto;
+  width: calc(100% + 20px);
+  border-radius: 10px;
+  box-shadow: 200px 10px 100px rgba(0, 0, 0, 0.8),
+    -200px 10px 100px rgba(0, 0, 0, 0.8), 200px -10px 100px rgba(0, 0, 0, 0.8),
+    -200px -10px 100px rgba(0, 0, 0, 0.8);
+
+  @media (max-width: ${mediaQuery.breakpoints.mobileMax}px) {
+    max-width: 300px;
+  }
+  @media (max-width: ${mediaQuery.breakpoints.tabletVerticalMax}px) {
+    max-width: 780px;
+  }
 `;
 
 export const BackdropWrapper = styled.div`
-  max-width: 1280px;
-  margin: 0 auto;
+  padding: 56px 0;
+  position: absolute;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+
+  @media (max-width: ${mediaQuery.breakpoints.mobileMax}px) {
+    padding: 8px 16px;
+    gap: 4px;
+  }
+
+  @media (max-width: ${mediaQuery.breakpoints.tabletVerticalMax}px) {
+    padding: 8px 16px;
+    gap: 8px;
+  }
 `;
 
 export const BackdropTitle = styled.h2`
   color: ${({ theme }) => theme.color.white};
-  max-width: 1280px;
-  margin: 0 auto;
+  font-size: 64px;
+  font-weight: bold;
+  margin: 0;
+
+  @media (max-width: ${mediaQuery.breakpoints.mobileMax}px) {
+    font-size: 24px;
+  }
+
+  @media (max-width: ${mediaQuery.breakpoints.tabletVerticalMax}px) {
+    font-size: 48px;
+  }
 `;
