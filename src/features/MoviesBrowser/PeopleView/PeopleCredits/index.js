b251c164c546442ddb295d5eb07f6b0d890e951f
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-05-26 11:37:08 +0200

    peopleCredits created

---
+++ src/features/MoviesBrowser/PeopleView/PeopleCredits/index.js
@@ -0,0 +1,55 @@
+import { useSelector } from "react-redux";
+import {
+  PeopleCreditsContent,
+  PeopleCreditsData,
+  PeopleCreditsDefaultPoster,
+  PeopleCreditsHeader,
+  PeopleCreditsMovieTitle,
+  PeopleCreditsPoster,
+  PeopleCreditsPosterIcon,
+  PeopleCreditsProductionYear,
+  PeopleCreditsTile,
+  PeopleCreditsWrapper,
+} from "./styled";
+import { selectPeopleCreditsState } from "./peopleCreditsSlice";
+
+const PeopleCredits = ({ type }) => {
+  const { creditsData } = useSelector(selectPeopleCreditsState); // Destrukturyzacja `creditsData` z selektora
+  console.log("PeopleCredits", creditsData);
+
+  const creditsArray = creditsData ? Object.values(creditsData) : [];
+  return (
+    <>
+      {type && <PeopleCreditsHeader>{type}</PeopleCreditsHeader>}
+      <PeopleCreditsWrapper>
+        {creditsArray.map((person) => (
+          <PeopleCreditsTile key={person.id}>
+            {person.poster_path ? (
+              <PeopleCreditsPoster
+                src={`https://image.tmdb.org/t/p/w185/${person.poster_path}`}
+                alt="Person photo"
+              />
+            ) : (
+              <PeopleCreditsDefaultPoster>
+                <PeopleCreditsPosterIcon />
+              </PeopleCreditsDefaultPoster>
+            )}
+            <PeopleCreditsContent>
+              <PeopleCreditsMovieTitle>
+                {person.original_title}
+              </PeopleCreditsMovieTitle>
+              <PeopleCreditsData>
+                {person.character || person.job}
+              </PeopleCreditsData>
+              <PeopleCreditsProductionYear>
+                {new Date(person.release_date).getFullYear()}
+              </PeopleCreditsProductionYear>
+            </PeopleCreditsContent>
+          </PeopleCreditsTile>
+        ))}
+      </PeopleCreditsWrapper>
+    </>
+  );
+};
+
+export default PeopleCredits;
