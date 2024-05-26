602f24f3a4a355d780dec4922ae88597e17a8e6f
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-05-26 11:16:37 +0200

    People Details component created, it's contains index, styles getApi, Saga and Slice

---
+++ src/features/MoviesBrowser/PeopleView/PeopleDetails/index.js
@@ -0,0 +1,44 @@
+import { useSelector } from "react-redux";
+import { selectPeopleDetailsState } from "./peopleDetailsSlice";
+import {
+  PeopleDetailsBiography,
+  PeopleDetailsBirthDate,
+  PeopleDetailsBirthDateContent,
+  PeopleDetailsBirthPlace,
+  PeopleDetailsBirthPlaceContent,
+  PeopleDetailsContent,
+  PeopleDetailsData,
+  PeopleDetailsHeader,
+  PeopleDetailsPhoto,
+  PeopleDetailsWrapper,
+} from "./styled";
+
+const PeopleDetails = () => {
+  const { details } = useSelector(selectPeopleDetailsState);
+  console.log("PeopleDetails", details);
+
+  return (
+    <PeopleDetailsWrapper>
+      <PeopleDetailsPhoto
+        src={`https://image.tmdb.org/t/p/w185/${details.profile_path}`}
+        alt={details.name}
+      />
+      <PeopleDetailsContent>
+        <PeopleDetailsHeader>{details.name}</PeopleDetailsHeader>
+        <PeopleDetailsData>
+          <PeopleDetailsBirthDate>Date of birth:</PeopleDetailsBirthDate>
+          <PeopleDetailsBirthDateContent>
+            {details.birthday}
+          </PeopleDetailsBirthDateContent>
+          <PeopleDetailsBirthPlace>Place of birth:</PeopleDetailsBirthPlace>
+          <PeopleDetailsBirthPlaceContent>
+            {details.place_of_birth}
+          </PeopleDetailsBirthPlaceContent>
+        </PeopleDetailsData>
+      </PeopleDetailsContent>
+      <PeopleDetailsBiography>{details.biography}</PeopleDetailsBiography>
+    </PeopleDetailsWrapper>
+  );
+};
+
+export default PeopleDetails;
---
+++ src/features/MoviesBrowser/PeopleView/PeopleDetails/peopleDetailsAPI.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const API_KEY = "b024809636d033af8d3a072db3fe8b5d";
+
+const options = {
+  method: "GET",
+  url: `https://api.themoviedb.org/3/person/976`,
+  params: { language: "en-US", api_key: API_KEY },
+  headers: {
+    accept: "application/json",
+  },
+};
+
+export const getPeopleDetails = async () => {
+  const response = await axios.request(options);
+  console.log("getPeopleDetails", response.data);
+  return response.data;
+};
