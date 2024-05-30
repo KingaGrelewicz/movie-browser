2be0ed5b06d098052c896adc914d9e0c324989ff
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-05-30 16:09:03 +0200

    PopularPeopleView added as main view of popular people

--- src/core/App/App.js
+++ src/core/App/App.js
@@ -2,25 +2,25 @@ import { PageStatus } from "../../features";
 import Navigation from "../../common/Navigation";
 import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import { toPeople } from "./routes";
-import PopularPeople from "../../features/MoviesBrowser/PopularPeople";
 import MovieDetails from "../../features/MoviesBrowser/MovieDetails";
+import PopularPeopleView from "../../features/MoviesBrowser/PopularPeopleView";
 
 export const App = () => (
-	<HashRouter>
-		<Navigation />
-		<Switch>
-			<Route path="/movies/:id">
-				<MovieDetails />
-			</Route>
-			<Route path="/movies">
-				<PageStatus />
-			</Route>
-			<Route path={toPeople()}>
-				<PopularPeople />
-			</Route>
-			<Route path="/">
-				<Redirect to="/movies" />
-			</Route>
-		</Switch>
-	</HashRouter>
+  <HashRouter>
+    <Navigation />
+    <Switch>
+      <Route path="/movies/:id">
+        <MovieDetails />
+      </Route>
+      <Route path="/movies">
+        <PageStatus />
+      </Route>
+      <Route path={toPeople()}>
+        <PopularPeopleView />
+      </Route>
+      <Route path="/">
+        <Redirect to="/movies" />
+      </Route>
+    </Switch>
+  </HashRouter>
 );
