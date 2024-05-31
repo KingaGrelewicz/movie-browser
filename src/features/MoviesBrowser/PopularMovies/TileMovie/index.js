ab317e81c4c31ff4fac8c577c8552105dfce6eb9
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-05-31 15:05:01 +0200

    TileMovie component updated, Genre added

--- src/features/MoviesBrowser/PopularMovies/TileMovie/index.js
+++ src/features/MoviesBrowser/PopularMovies/TileMovie/index.js
@@ -9,31 +9,51 @@ import {
   AverageRating,
   StyleStarIcon,
 } from "./styled";
-import { GenresList } from "../../MoviesGenre/index.js";
+import Genres from "../../Genres/index.js";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { fetchGenres } from "../../Genres/genresSlice.js";
 
 export const TileMovie = ({ repositories }) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchGenres());
+  }, [dispatch]);
+
   const movies = Object.values(repositories.results);
-  console.log(movies)
+  console.log(movies);
 
   return (
-      <>
-          {movies.map(({id, title, release_date, poster_path, vote_count, vote_average, genre_ids }) => (
-
-              <Container key={id}>
-                  <Image src={`https://image.tmdb.org/t/p/original/${poster_path}`} alt="" />
-                  <Description>
-                      <DescriptionTitle>{title}</DescriptionTitle>
-                      <Data>{release_date}</Data>
-                      <GenresList genre_ids={genre_ids}/>
-                      <Rating>
-                          <StyleStarIcon />
-                          <AverageRating>{vote_average.toFixed(1)}</AverageRating>
-                          <NumberOfRating>{vote_count} votes</NumberOfRating>
-                      </Rating>
-                  </Description>
-              </Container>
-
-          ))}
-      </>
-  )
+    <>
+      {movies.map(
+        ({
+          id,
+          title,
+          release_date,
+          poster_path,
+          vote_count,
+          vote_average,
+          genre_ids,
+        }) => (
+          <Container key={id}>
+            <Image
+              src={`https://image.tmdb.org/t/p/original/${poster_path}`}
+              alt=""
+            />
+            <Description>
+              <DescriptionTitle>{title}</DescriptionTitle>
+              <Data>{release_date}</Data>
+              <Genres genreIds={genre_ids} />
+              <Rating>
+                <StyleStarIcon />
+                <AverageRating>{vote_average.toFixed(1)}</AverageRating>
+                <NumberOfRating>{vote_count} votes</NumberOfRating>
+              </Rating>
+            </Description>
+          </Container>
+        )
+      )}
+    </>
+  );
 };
