9552efb8b23e6e1967006eedf43c08b69225230f
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-05-31 14:43:59 +0200

    genresSaga added to rootSaga

--- src/core/App/rootSaga.js
+++ src/core/App/rootSaga.js
@@ -6,6 +6,7 @@ import { movieBrowserSaga } from "../../features/MoviesBrowser/movieBrowserSaga"
 import { movieGenreSaga } from "../../features/MoviesBrowser/MoviesGenre/movieGenreSaga";
 import { peopleDetailsSaga } from "../../features/MoviesBrowser/PeopleView/PeopleDetails/peopleDetailsSaga";
 import { peopleCreditsSaga } from "../../features/MoviesBrowser/PeopleView/PeopleCredits/peopleCreditsSaga";
+import { genresSaga } from "../../features/MoviesBrowser/Genres/genresSaga";
 
 export default function* rootSaga() {
   yield all([
@@ -16,5 +17,6 @@ export default function* rootSaga() {
     movieCreditsSaga(),
     peopleDetailsSaga(),
     peopleCreditsSaga(),
+    genresSaga(),
   ]);
 }
