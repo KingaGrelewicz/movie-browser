64aaea79b1a8986fb7363cbe80b270db114bdecc
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-06-01 11:11:43 +0200

    ErrorPage adjusted to router version

--- src/features/ErrorPage/index.js
+++ src/features/ErrorPage/index.js
@@ -1,21 +1,17 @@
-import { toHomePage } from "../../core/App/routes";
-import { useNavigate } from "react-router-dom";
 import { ReactComponent as ErrorIcon } from "./../../images/error.svg";
-import { Info, Text, Wrapper, Button } from "./styled";
+import { Info, Text, Wrapper, ErrorNavLink } from "./styled";
 
-export default () => {
-	const navigate = useNavigate();
-
-	return (
-		<Wrapper>
-			<ErrorIcon />
-			<Text>Opps! Something went wrong...</Text>
-			<Info>
-				Please check your network connection <br /> and try again
-			</Info>
-			<Button onClick={() => navigate(toHomePage())} >
-				Back to home page
-			</Button>
-		</Wrapper>
-	);
+const Error = () => {
+  return (
+    <Wrapper>
+      <ErrorIcon />
+      <Text>Opps! Something went wrong...</Text>
+      <Info>
+        Please check your network connection <br /> and try again
+      </Info>
+      <ErrorNavLink to="/movies">Back to home page</ErrorNavLink>
+    </Wrapper>
+  );
 };
+
+export default Error;
