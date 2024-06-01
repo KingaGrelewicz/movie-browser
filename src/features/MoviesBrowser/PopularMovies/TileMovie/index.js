79a2decd9fa4f1aee8a020a18d1603f0c691decf
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-06-01 21:21:57 +0200

    ratingData added to TileMovie

--- src/features/MoviesBrowser/PopularMovies/TileMovie/index.js
+++ src/features/MoviesBrowser/PopularMovies/TileMovie/index.js
@@ -1,23 +1,20 @@
 import {
-    Container,
-    Image,
-    Description,
-    DescriptionTitle,
-    Data,
-    Rating,
-    NumberOfRating,
-    AverageRating,
-    StyleStarIcon,
-    Links
+  Container,
+  Image,
+  Description,
+  DescriptionTitle,
+  Data,
+  Links,
 } from "./styled";
 import Genres from "../../Genres/index.js";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { fetchGenres } from "../../Genres/genresSlice.js";
+import Rating from "../../Rating";
 
 export const TileMovie = ({ repositories }) => {
   const dispatch = useDispatch();
-
+ 
   useEffect(() => {
     dispatch(fetchGenres());
   }, [dispatch]);
@@ -43,14 +40,15 @@ export const TileMovie = ({ repositories }) => {
               alt=""
             />
             <Description>
-              <DescriptionTitle>{title}</DescriptionTitle>
+              <DescriptionTitle>
+                <Links to={`/movies/${id}`}>{title}</Links>
+              </DescriptionTitle>
               <Data>{release_date}</Data>
               <Genres genreIds={genre_ids} />
-              <Rating>
-                <StyleStarIcon />
-                <AverageRating>{vote_average.toFixed(1)}</AverageRating>
-                <NumberOfRating>{vote_count} votes</NumberOfRating>
-              </Rating>
+              <Rating
+                variant="movies"
+                ratingData={{ vote_average, vote_count }}
+              />
             </Description>
           </Container>
         )
