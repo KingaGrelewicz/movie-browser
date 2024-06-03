f40b1479d3c11d4809952ab67e560c65e324f2bf
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-06-03 20:35:54 +0200

    Loading and Error Views added to movieDetails

--- src/features/MoviesBrowser/MovieDetails/index.js
+++ src/features/MoviesBrowser/MovieDetails/index.js
@@ -1,23 +1,31 @@
 import { useDispatch, useSelector } from "react-redux";
-import {fetchMovieDetails,selectMovieDetailsState,} from "./movieDetailsSlice";
+import {
+  fetchMovieDetails,
+  selectMovieDetailsState,
+} from "./movieDetailsSlice";
 import { useEffect } from "react";
 import Details from "./Details";
 import Backdrop from "./Backdrop";
-import {fetchMovieCredits,selectMovieCreditsState,} from "./Credits/creditsSlice";
+import {
+  fetchMovieCredits,
+  selectMovieCreditsState,
+} from "./Credits/creditsSlice";
 import Credits from "./Credits";
 import { MovieDetailsWrapper } from "./styled";
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useParams } from "react-router-dom";
+import { Loading } from "../../LoadingPage";
+import Error from "../../ErrorPage";
 
 const MovieDetails = () => {
   const dispatch = useDispatch();
   const movieData = useSelector(selectMovieDetailsState);
   const movieCredits = useSelector(selectMovieCreditsState);
-  console.log(movieCredits.credits)
+  console.log(movieCredits.credits);
   const { cast, crew } = movieCredits.credits || {};
 
   const movieDetails = movieData.details;
   const params = useParams();
-  const movieIp = params.id
+  const movieIp = params.id;
 
   useEffect(() => {
     dispatch(fetchMovieDetails(movieIp));
@@ -26,20 +34,16 @@ const MovieDetails = () => {
 
   return (
     <>
-      {movieData.status && movieCredits.status === "loading" && (
-        <p>Loading...</p>
-      )}
+      {movieData.status && movieCredits.status === "loading" && <Loading />}
       {movieData.status && movieCredits.status === "success" && (
         <MovieDetailsWrapper>
-          <Backdrop movieDetails={movieDetails}/>
-          <Details movieDetails={movieDetails}/>
+          <Backdrop movieDetails={movieDetails} />
+          <Details movieDetails={movieDetails} />
           <Credits credits={cast} type="Cast" />
           <Credits credits={crew} type="Crew" />
         </MovieDetailsWrapper>
       )}
-      {movieData.status && movieCredits.status === "error" && (
-        <p>Error fetching data</p>
-      )}
+      {movieData.status && movieCredits.status === "error" && <Error />}
     </>
   );
 };
