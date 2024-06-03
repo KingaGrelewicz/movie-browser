3c3d18d7c7ddbebca6de3ebcfc0276a874e4d910
Maja Celej <cmaja1825@gmail.com>
2024-06-03 20:34:29 +0200

    added logic of search engine

--- src/common/Navigation/Search/index.js
+++ src/common/Navigation/Search/index.js
@@ -1,10 +1,29 @@
-import { Wrapper, Input, SearchPic } from "./styled";
+import { useLocation } from 'react-router-dom';
+import { Wrapper, Input } from "./styled";
+import { useEffect, useState } from 'react';
+import { toPeople } from "./../../../core/App/routes";
+import { useOnInputChange } from './useOnInputChange';
 
 const Search = () => {
+  const location = useLocation();
+  const query = new URLSearchParams(location.search).get("szukaj") || "";
+
+  const [inputValue, setInputValue] = useState(query);
+
+  const onInputChange = useOnInputChange(setInputValue);
+  const isSearchFoePeople = location.pathname.startsWith(toPeople());
+  
+  useEffect(() => {
+    setInputValue(query);
+  }, [query]);
+
   return (
     <Wrapper>
-      <SearchPic />
-      <Input placeholder="Search for movies..." />
+      <Input 
+        onChange={({ target: { value } }) => onInputChange({ newValue: value, isSearchFoePeople})}
+        value={inputValue}
+        placeholder={`Search for ${isSearchFoePeople ? "people" : "movies"}...`} 
+      />
     </Wrapper>
   );
 };
--- src/features/MoviesBrowser/PopularMovies/Popular/index.js
+++ src/features/MoviesBrowser/PopularMovies/Popular/index.js
@@ -1,12 +1,23 @@
 import { TileMovie } from "../TileMovie";
 import { Title, Movies } from "./styled";
 
-export const Popular = ({ repositories }) => (
-    <>
-        <Title>Popular movies</Title>
-        <Movies>
-            <TileMovie repositories={repositories} />
-        </Movies>
-    </>
+export const Popular = ({ repositories, inputValue}) => {
+	const films = Object.values(repositories.results);
+	console.log(films);
 
-)
+	const filteredMovies = films.filter(movie =>
+        movie.title.includes(inputValue)
+    );
+        
+
+	return (
+			<>
+				<Title>Popular movies</Title>
+				<Movies>
+				{filteredMovies.map((movie) => (
+                    <TileMovie key={movie.id} repositories={movie} />
+                ))}
+				</Movies>
+			</>
+	);
+};
