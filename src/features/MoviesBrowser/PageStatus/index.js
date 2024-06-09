43e7c906ef48ca78213766d4c7ac4c649576baec
Maja Celej <cmaja1825@gmail.com>
2024-06-09 14:45:12 +0200

    brought back pagination

--- src/features/MoviesBrowser/PageStatus/index.js
+++ src/features/MoviesBrowser/PageStatus/index.js
@@ -1,24 +1,30 @@
 import { useSelector } from "react-redux";
-import { selectRepositories, selectRepositoriesStatus } from "../movieBrowserSlice";
+import {
+  selectRepositories,
+  selectRepositoriesStatus,
+} from "../movieBrowserSlice";
 import { Container } from "./styled";
 import { PageView } from "../../PageView";
+import { Paginations } from "../../Pagination";
 import { selectPages, fetchRepositories } from "../movieBrowserSlice";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { setPages } from "../movieBrowserSlice";
 
 export const PageStatus = () => {
-	const dispatch = useDispatch();
-	const repositoriesStatus = useSelector(selectRepositoriesStatus);
-	const repositories = useSelector(selectRepositories);
-	const pages = useSelector(selectPages);
+  const dispatch = useDispatch();
+  const repositoriesStatus = useSelector(selectRepositoriesStatus);
+  const repositories = useSelector(selectRepositories);
+  const pages = useSelector(selectPages);
 
-	useEffect(() => {
-		dispatch(fetchRepositories(pages));
-	}, [pages]);
+  useEffect(() => {
+    dispatch(fetchRepositories(pages));
+  }, [pages]);
 
-	return (
-		<Container>
-			<PageView status={repositoriesStatus} repositories={repositories} />
-		</Container>
-	);
+  return (
+    <Container>
+      <PageView status={repositoriesStatus} repositories={repositories} />
+      <Paginations pages={pages} setPages={setPages} />
+    </Container>
+  );
 };
