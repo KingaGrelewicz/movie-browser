15d18f84f5137b70c344a86c9e26b88fd3d75a0f
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-06-10 18:48:43 +0200

    PopularPeople updated

--- src/features/MoviesBrowser/PopularPeopleView/PopularPeople/index.js
+++ src/features/MoviesBrowser/PopularPeopleView/PopularPeople/index.js
@@ -15,45 +15,50 @@ import { Paginations } from "../../../Pagination";
 import { setPages } from "./popularPeopleSlice";
 import { NoResultView } from "../../../NoResultView";
 
-
 const PopularPeople = ({ pages, query }) => {
-	const popularPeople = useSelector((state) => selectPeopleByQuery(state, query));
-	const numberOfPeople = popularPeople.length;
+  const popularPeople = useSelector((state) =>
+    selectPeopleByQuery(state, query)
+  );
+  const numberOfPeople = popularPeople.length;
 
-	const title = query ? `Searches for "${query}" (${numberOfPeople} people)` : "POPULAR PEOPLE";
+  const title = query
+    ? `Searches for "${query}" (${numberOfPeople} people)`
+    : "Popular People";
 
-	return (
-		<>
-			{popularPeople.length > 0 ? (
-				<>
-					<PopularPeopleHeaderWrapper>
-						<PopularPeopleHeader>{title}</PopularPeopleHeader>
-					</PopularPeopleHeaderWrapper>
-					<PopularPeopleWrapper>
-						{popularPeople.map((people) => (
-							<PopularPeopleTile key={people.id}>
-								{people.profile_path ? (
-									<Links to={`/people/${people.id}`}>
-									<PopularPeoplePhoto src={`https://image.tmdb.org/t/p/w185/${people.profile_path}`} alt={people.name} />
-									</Links>
-								) : (
-									<PopularPeopleDefaultPoster>
-										<PopularPeoplePosterIcon />
-									</PopularPeopleDefaultPoster>
-								)}
-								
-									<PopularPeopleName>{people.name}</PopularPeopleName>
-								
-							</PopularPeopleTile>
-						))}
-					</PopularPeopleWrapper>
-					<Paginations pages={pages} setPages={setPages} />
-				</>
-			) : (
-				<NoResultView query={query} />
-			)}
-		</>
-	);
+  return (
+    <>
+      {popularPeople.length > 0 ? (
+        <>
+          <PopularPeopleHeaderWrapper>
+            <PopularPeopleHeader>{title}</PopularPeopleHeader>
+          </PopularPeopleHeaderWrapper>
+          <PopularPeopleWrapper>
+            {popularPeople.map((people) => (
+              <PopularPeopleTile key={people.id}>
+                {people.profile_path ? (
+                  <Links to={`/people/${people.id}`}>
+                    <PopularPeoplePhoto
+                      src={`https://image.tmdb.org/t/p/w185/${people.profile_path}`}
+                      alt={people.name}
+                    />
+                  </Links>
+                ) : (
+                  <PopularPeopleDefaultPoster>
+                    <PopularPeoplePosterIcon />
+                  </PopularPeopleDefaultPoster>
+                )}
+
+                <PopularPeopleName>{people.name}</PopularPeopleName>
+              </PopularPeopleTile>
+            ))}
+          </PopularPeopleWrapper>
+          <Paginations pages={pages} setPages={setPages} />
+        </>
+      ) : (
+        <NoResultView query={query} />
+      )}
+    </>
+  );
 };
 
 export default PopularPeople;
